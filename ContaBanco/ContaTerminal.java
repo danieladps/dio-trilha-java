3913fad5a5bde85f4c175a1e5e9c93ee46447347
Daniela <danielapereira5166@gmail.com>
2025-07-09 03:06:18 -0300

    Atualizado

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello world");
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("********** Bem - vindo ao Banco DIO **********");
 
+        System.out.println("Digite seu Nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite sua conta");
+        int numero = scanner.nextInt();
+
+        float saldo = 500.20F;
+
+        System.out.println("Olá " +nome+ ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
